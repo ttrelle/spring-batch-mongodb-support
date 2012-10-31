f377d4814169da679702d994962d5373008ecd1f
Tobias Trelle <tobias.trelle@codecentric.de>
2012-10-31 14:16:11 +0100

    added package level documentation

---
+++ src/main/java/org/springframework/batch/item/mongodb/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * <a href="http://www.mongodb.org">MongoDB</a> support for the 
+ * <a href="http://static.springsource.org/spring-batch">Spring Batch</a> framework.
+ * 
+ * @author <a href="mailto:tobias.trelle">Tobias Trelle</a>
+ */
+package org.springframework.batch.item.mongodb;
